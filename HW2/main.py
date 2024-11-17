a721059cec76d0005a668df55ffa49936bd8a742
haya <66245708+haya-hyh@users.noreply.github.com>
2024-11-17 19:43:30 +0800

    Update main.py

--- HW2/main.py
+++ HW2/main.py
@@ -3,8 +3,8 @@ import readfile
 import aprioritid as aptid
 import time
 transactions = readfile.read_dataset('T10.dat')
-min_support = 50
-min_confidence = 0.8
+min_support = 20
+min_confidence = 0.0
 
 # #apriori part
 # Start_time_apriori = time.time()
@@ -28,8 +28,8 @@ Stop_time_aprioritid = time.time()
 for i, frequent_itemsets in enumerate(apriori_tid.frequent_itemset_list):
     print(f"Frequent {i + 1}-itemsets: {frequent_itemsets}")
 
-
-
+print(apriori_tid.rules)
 
 # to do list 
 # compare time between ap and aptid (select a appropriate dataset part)
+# need a dataset and good parameter to test generate rule function
