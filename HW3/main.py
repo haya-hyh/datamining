652895f9aab06c39092ea011908478728fdb7064
haya <66245708+haya-hyh@users.noreply.github.com>
2024-11-24 20:05:04 +0800

    Update main.py

--- HW3/main.py
+++ HW3/main.py
@@ -8,13 +8,13 @@ edges = rf.read_dataset(dataset_path)
 # Initialize algorithms
 sample_size = 1000
 triest_base = cs.TriestBase(sample_size)
-# triest_impr = cs.TriestImpr(sample_size)
+triest_impr = cs.TriestImpr(sample_size)
 
 # Process the stream
 for edge in edges:
     triest_base.process_edge(edge)
-    # triest_impr.process_edge(edge)
+    triest_impr.process_edge(edge)
 
 # Print results
 print("TRIÈST-BASE Triangle Estimate:", triest_base.get_estimate())
-# print("TRIÈST-IMPR Triangle Estimate:", triest_impr.get_estimate())
+print("TRIÈST-IMPR Triangle Estimate:", triest_impr.get_estimate())
